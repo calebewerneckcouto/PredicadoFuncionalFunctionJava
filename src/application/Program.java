920c78af1da9d7d94e17f08343d5f04dd6b0ad65
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-07 13:31:22 -0300

    expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -25,8 +26,11 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		
+		Function<Product, String> prods = p-> p.getName().toUpperCase();
 
-	  List<String> name =	list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+	  List<String> name =	list.stream().map(prods).collect(Collectors.toList());
 	  
 	  
 	  
